fbf3d0e6bf8d7568d111af7d678ab59c5059ae63
Kim Seung-Been <mainlib990@gmail.com>
2024-03-26 00:30:25 +0900

    feat: Add memories-score

--- memories-score/src/main/java/Solution.java
+++ memories-score/src/main/java/Solution.java
@@ -1,3 +1,42 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
 
+    public int[] solution(String[] names, int[] yearnings, String[][] photos) {
+        MemoriesScore memoriesScore = MemoriesScore.of(names, yearnings);
+
+        return memoriesScore.scores(photos);
+    }
+
+    private static class MemoriesScore {
+
+        private final Map<String, Integer> nameToYearning;
+
+        public MemoriesScore(Map<String, Integer> nameToYearning) {
+            this.nameToYearning = nameToYearning;
+        }
+
+        public static MemoriesScore of(String[] names, int[] yearnings) {
+            Map<String, Integer> nameToYearning = IntStream.range(0, Math.min(names.length, yearnings.length))
+                    .mapToObj(i -> Map.entry(names[i], yearnings[i]))
+                    .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+
+            return new MemoriesScore(nameToYearning);
+        }
+
+        public int[] scores(String[][] photos) {
+            return Arrays.stream(photos)
+                    .mapToInt(this::score)
+                    .toArray();
+        }
+
+        public int score(String... photos) {
+            return Arrays.stream(photos)
+                    .mapToInt(photo -> nameToYearning.getOrDefault(photo, 0))
+                    .sum();
+        }
+    }
 }
--- memories-score/src/test/java/SolutionTest.java
+++ memories-score/src/test/java/SolutionTest.java
@@ -1,5 +1,50 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
+    @ParameterizedTest
+    @MethodSource
+    void testSolution(
+            String[] names,
+            int[] yearnings,
+            String[][] photos,
+            int[] expectedResult) {
+        int[] actualResult = new Solution().solution(names, yearnings, photos);
+
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    private static Stream<Arguments> testSolution() {
+        return Stream.of(
+                Arguments.of(
+                        new String[]{"may", "kein", "kain", "radi"},
+                        new int[]{5, 10, 1, 3},
+                        new String[][]{
+                                {"may", "kein", "kain", "radi"},
+                                {"may", "kein", "brin", "deny"},
+                                {"kon", "kain", "may", "coni"}},
+                        new int[]{19, 15, 6}),
+                Arguments.of(
+                        new String[]{"kali", "mari", "don"},
+                        new int[]{11, 1, 55},
+                        new String[][]{
+                                {"kali", "mari", "don"},
+                                {"pony", "tom", "teddy"},
+                                {"con", "mona", "don"}},
+                        new int[]{67, 0, 55}),
+                Arguments.of(
+                        new String[]{"may", "kein", "kain", "radi"},
+                        new int[]{5, 10, 1, 3},
+                        new String[][]{
+                                {"may"},
+                                {"kein", "deny", "may"},
+                                {"kon", "coni"}},
+                        new int[]{5, 15, 0}));
+    }
 }
